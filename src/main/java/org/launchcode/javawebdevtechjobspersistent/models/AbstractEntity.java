228446da0e92b17d8c304556b9cb73ecdaffabde
franco <franco_cannistra@comcast.com>
2020-05-26 13:39:24 -0400

    Updated Gradle settings AbstratEntity

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,12 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Cannot leave blank.")
+    @Size(max = 75, message =  "Your name must be larger than 50 characters.")
     private String name;
 
     public int getId() {
@@ -16,7 +25,6 @@ public abstract class AbstractEntity {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
