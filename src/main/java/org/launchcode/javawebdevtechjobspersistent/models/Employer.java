e90e0c78e31ffde894016fbc8cc4562ed81dc4e1
franco <franco_cannistra@comcast.com>
2020-05-26 15:32:49 -0400

    Data Layer Complete

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
+import javax.persistence.Entity;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotNull(message = "Location is Required")
+    @Size(min = 3, max = 100)
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
 
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
+
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Please include description.")
+    @Size(max = 250)
+    private String description;
+
+    public Skill(){}
+
+    public Skill(String description){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
